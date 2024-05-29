abf93c65e139b686cc4b9fbb5366b6545e32f5d4
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-05-29 17:15:14 -0300

    fix issue with TextureLoader

--- components/Earth/Earth.tsx
+++ components/Earth/Earth.tsx
@@ -3,8 +3,8 @@
 import { Canvas, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
 import { useScroll, useSpring } from "framer-motion";
-import { TextureLoader } from "three/src/loaders/TextureLoader";
 import { motion } from "framer-motion-3d";
+import { TextureLoader } from "three";
 
 export default function Earth() {
   const scene = useRef(null);
@@ -25,8 +25,8 @@ export default function Earth() {
 
   return (
     <Canvas ref={scene}>
-      <ambientLight intensity={0.1} />
-      <directionalLight intensity={3.5} position={[1, 0, -0.25]} />
+      <ambientLight intensity={0.75} />
+      <directionalLight intensity={7.5} position={[6, 0, -0.5]} />
       <motion.mesh scale={1.5} rotation-y={smoothRotation} position={[0, 0, 0]}>
         <sphereGeometry args={[1, 64, 64]} />
         <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
